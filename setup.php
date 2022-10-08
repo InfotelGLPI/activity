bb3612b8517a482c8de023276953c8ab26d2b3e5
tsmr <thetsmr@gmail.com>
2022-10-08 14:40:04 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -24,7 +24,7 @@
  --------------------------------------------------------------------------
 */
 
-define('PLUGIN_ACTIVITY_VERSION', '3.1.1');
+define('PLUGIN_ACTIVITY_VERSION', '3.1.2');
 
 if (!defined("PLUGIN_ACTIVITY_DIR")) {
    define("PLUGIN_ACTIVITY_DIR", Plugin::getPhpDir("activity"));
