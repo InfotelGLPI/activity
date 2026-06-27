882e20ade55381eda81ea148715fc2c03b91f39d
Thetsmr <thetsmr@gmail.com>
2026-06-27 10:44:13 +0200

    Update license

--- setup.php
+++ setup.php
@@ -14,7 +14,7 @@
 
  activity is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
+ the Free Software Foundation; either version 3 of the License, or
  (at your option) any later version.
 
  activity is distributed in the hope that it will be useful,
@@ -212,7 +212,7 @@ function plugin_version_activity()
         'name' => _n('Activity', 'Activities', 2, 'activity'),
         'version' => PLUGIN_ACTIVITY_VERSION,
         'author'       => "<a href='https://blogglpi.infotel.com'>Infotel</a>, Xavier CAILLAUD",
-        'license' => 'GPLv2+',
+        'license' => 'GPLv3+',
         'homepage' => '',
         'requirements' => [
             'glpi' => [
