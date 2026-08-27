d4e7da969bfa752e060371748488e0f35dcc6435
Thetsmr <thetsmr@gmail.com>
2026-08-27 10:33:29 +0200

    CS Fixer

--- front/menu.php
+++ front/menu.php
@@ -37,7 +37,7 @@ $can = Session::haveRight("plugin_activity", READ);
 $canholiday = Session::haveRight("plugin_activity_can_requestholiday", 1);
 $canvalidateholiday = Session::haveRight("plugin_activity_can_validate", 1);
 
-if (!$can 
+if (!$can
     && !$canholiday
     && !$canvalidateholiday) {
     throw new AccessDeniedHttpException();
